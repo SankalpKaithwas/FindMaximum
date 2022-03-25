dfe15ac2ab1ac8b334554812052dc4acda6c26a0
unknown <kaithwas.697@gmail.com>
2022-03-25 20:10:38 +0530

    Added feature to check maximum among three floats

---
+++ FindMaximum/FindMaximum/FindMaximumNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace FindMaximum
+{
+    public class FindMaximumNumber
+    {
+        public static int GetMaximum(int numberOne, int numberTwo, int numberThree)
+        {
+            int max = numberOne;
+            if (numberTwo.CompareTo(max) > 0)
+            {
+                max = numberTwo;
+            }
+            if (numberThree.CompareTo(max) > 0)
+            {
+                max = numberThree;
+            }
+            return max;
+        }
+        public static double GetMaximumFloat(double numberOne, double numberTwo, double numberThree)
+        {
+            double max = numberOne;
+            if (numberTwo.CompareTo(max) > 0)
+            {
+                max = numberTwo;
+            }
+            if (numberThree.CompareTo(max) > 0)
+            {
+                max = numberThree;
+            }
+            return max;
+        }
+    }
+}
--- FindMaximum/FindMaximum/Program.cs
+++ FindMaximum/FindMaximum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace FindMaximum
 {
     internal class Program
@@ -9,23 +10,16 @@ namespace FindMaximum
             int numberOne = 34;
             int numberTwo = 123;
             int numberThree = 19;
+            //UC1 Find maximum integer
+            int maximum = FindMaximumNumber.GetMaximum(numberOne, numberTwo, numberThree);
+            Console.WriteLine($"Maximum among [{numberOne}, {numberTwo}, " +
+                $"{numberThree}] is: {maximum}");
 
-            int maximum = GetMaximum(numberOne, numberTwo, numberThree);
-            Console.WriteLine(maximum);
+            // UC2 find Maximum Float
+            double maximumDouble = FindMaximumNumber.GetMaximumFloat(12.3, 23.4, 20);
+            Console.WriteLine($"Maximum among [12.3, 23.4, 20] is: {maximumDouble}");
 
         }
-        private static int GetMaximum(int numberOne, int numberTwo, int numberThree)
-        {
-            int max = numberOne;
-            if (numberTwo.CompareTo(max) > 0)
-            {
-                max = numberTwo;
-            }
-            if (numberThree.CompareTo(max) > 0)
-            {
-                max = numberThree;
-            }
-            return max;
-        }
+
     }
 }
