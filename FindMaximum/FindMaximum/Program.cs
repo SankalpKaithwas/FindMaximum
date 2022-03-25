d952256fb43a27b9c18094932452d90758c2cd46
unknown <kaithwas.697@gmail.com>
2022-03-25 18:04:14 +0530

    Added feature to check the largest number

---
+++ FindMaximum/FindMaximum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FindMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 34;
+            int numberTwo = 123;
+            int numberThree = 19;
+
+            int maximum = GetMaximum(numberOne, numberTwo, numberThree);
+            Console.WriteLine(maximum);
+
+        }
+        private static int GetMaximum(int numberOne, int numberTwo, int numberThree)
+        {
+            int max = numberOne;
+            if (numberTwo.CompareTo(max) > 0)
+            {
+                max = numberTwo;
+            }
+            if (numberThree.CompareTo(max) > 0)
+            {
+                max = numberThree;
+            }
+            return max;
+        }
+    }
+}
